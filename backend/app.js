3473d2cdc8558955605d27195d294315f8ece21f
ashutosh6565 <kumarashutosh7302401472.com>
2025-03-21 21:28:05 +0530

    user route and controller updated

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
-const dotenv  =require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
-const cors = require('cors');
-const app = express(); 
-const  connectToDb = require('./db/db');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connectToDb = require("./db/db");
+// const userController = require("./controllers/user.controller");
+const userRouter = require("./routes/user.routes");
 connectToDb();
 app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-
+app.use("/users", userRouter);
 
 module.exports = app;
