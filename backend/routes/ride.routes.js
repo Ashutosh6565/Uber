9ca85730b988eeba0119c0d5ff2e1bf1f32c8f3a
ashutosh6565 <kumarashutosh7302401472.com>
2025-05-12 23:55:16 +0530

    made the model in for the ride in backend

---
+++ backend/routes/ride.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator');
+
+router.post('/create', 
+    body('userId').isString().isLength({min:24, max:24}).notEmpty().withMessage('User ID is required'),
+    body('pickup').isString().isLength({min:3}).withMessage('Pickup location is required'),
+    body('destination').isString().isLength({min:3}).withMessage('Destination location is required'),
